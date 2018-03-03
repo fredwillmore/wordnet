c09634d7034b4f0d4b7aecf233cf0a85cb43c983
Fred Willmore <fredwillmore@gmail.com>
2018-03-03 14:20:13 -0800

    added private class Words for word lookup, implemented WordNet constructor

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,11 +1,36 @@
 import java.util.Iterator;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
 
 public class WordNet {
+
+    private Digraph G;
+    private Words words;
+
+    private class Words {
+        private String[] words;
+        public Words(In in) {
+            this.words = in.readAllLines();
+        }
+
+        public int size() {
+            return words.length;
+        }
+    }
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null)
             throw new java.lang.IllegalArgumentException();
+
+        words = new Words(new In(synsets));
+        G = new Digraph(words.size());
+        for (String line: new In(hypernyms).readAllLines()) {
+            String[] values = line.split(",");
+            int v = Integer.parseInt(values[0]);
+            int w = Integer.parseInt(values[1]);
+            G.addEdge(v, w);
+        }
     }
 
     // returns all WordNet nouns
@@ -46,6 +71,13 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
+        System.out.println("testConstructor: " + testConstructor("synsets3.txt", "hypernyms3InvalidCycle.txt"));
+    }
 
+    private static boolean testConstructor(String synsetsFile, String hypernymsFile) {
+        WordNet thinger = new WordNet("wordnet/" + synsetsFile, "wordnet/" + hypernymsFile);
+        System.out.println("# vertices: " + thinger.G.V());
+        System.out.println("# edges: " + thinger.G.E());
+        return thinger.getClass().getName() == "WordNet";
     }
 }
